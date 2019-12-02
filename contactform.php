e16469a02a6d8b6476971717a29d01397774a598
Cynthia <cyn.mart@outlook.com>
2019-12-02 03:14:20 -0800

    Conctact Page Testing

---
+++ contactform.php
@@ -0,0 +1,19 @@
+<?php
+
+if(isset($_POST['submit'])){
+	$name = $_POST['name'];
+	$mailFrom = $_POST['mail'];
+	$subject = $_POST['subject'];
+	$message = $_POST['message'];
+
+	$mailTo = "cyn.mart@outlook.com";
+	$headers = "From: ". $mailFrom;
+	$txt = "You have received an email from " . $name.".\n\n".$message;
+
+	mail($mailTo, $subject, $txt, $headers);
+	header("Location: index.html?mailsend");
+
+}
+
+
+?>
